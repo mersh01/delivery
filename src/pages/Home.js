c87bf67dd6a57a8cfd925732ad93618d443c7e9c
mersh01 <meraolfeye6@gmail.com>
2025-02-25 17:07:04 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Testimonials from '../components/Testimonials';
 import Hero from '../components/Hero';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
